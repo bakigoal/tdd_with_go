b72598b3984ab0f8c802aa79b39e70d31a18be46
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-03-23 13:29:32 +0300

    create new type Bitcoin from int

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,16 +1,18 @@
 package pointers
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // a pointer to wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	// struct pointers automatically deReferenced to
 	// (*w).balance += amount
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
