2e61564e5bae1b3f3ed9166c8b7ed075be38d657
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-01 19:22:20 +0300

    add spy to mocks of sleeper and writer

--- ch_09_mocking/countdown_test.go
+++ ch_09_mocking/countdown_test.go
@@ -6,23 +6,42 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-	Countdown(buffer, sleeper)
-	want := `3
+		Countdown(buffer, &SpyCountdownOperations{})
+		want := `3
 2
 1
 Go!`
-	assert.Equal(t, want, buffer.String())
-	assert.Equal(t, 3, sleeper.Calls)
+		assert.Equal(t, want, buffer.String())
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{write, sleep, write, sleep, write, sleep, write}
+
+		assert.Equal(t, want, spySleepPrinter.Calls)
+	})
 }
