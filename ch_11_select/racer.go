dfd584dfe5af72130b2bfdc97372d110319ba6cc
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-07 17:48:11 +0300

    make timeout duration configurable

--- ch_11_select/racer.go
+++ ch_11_select/racer.go
@@ -6,14 +6,20 @@ import (
 	"time"
 )
 
+var defaultTimeoutDuration = 10 * time.Second
+
 func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, defaultTimeoutDuration)
+}
+
+func ConfigurableRacer(a, b string, duration time.Duration) (winner string, err error) {
 	// select lets you wait on multiple channels
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(duration):
 		return "", fmt.Errorf("timeout waiting for %s and %s", a, b)
 	}
 }
--- ch_11_select/racer_test.go
+++ ch_11_select/racer_test.go
@@ -23,12 +23,12 @@ func TestRacer(t *testing.T) {
 		assert.Equal(t, want, got)
 	})
 	t.Run("error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(11 * time.Second)
+		serverA := makeDelayedServer(11 * time.Millisecond)
+		serverB := makeDelayedServer(11 * time.Millisecond)
 		defer serverA.Close()
 		defer serverB.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 10*time.Millisecond)
 		assert.Error(t, err)
 	})
 }
