aa1835688ae0822f8fc85b38598c45d58b0ef4dd
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-11 11:01:31 +0300

    refactoring

--- ch_12_reflection/walker.go
+++ ch_12_reflection/walker.go
@@ -5,22 +5,17 @@ import "reflect"
 func Walk(x interface{}, f func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			Walk(val.Field(i).Interface(), f)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			Walk(val.Index(i).Interface(), f)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			f(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), f)
-		}
+	case reflect.String:
+		f(val.String())
 	}
 }
 
