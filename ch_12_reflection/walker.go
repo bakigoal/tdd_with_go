9f507228e03fd08159455fe26d35891b1c2333fc
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-11 10:45:21 +0300

    refactoring: use switch

--- ch_12_reflection/walker.go
+++ ch_12_reflection/walker.go
@@ -8,11 +8,10 @@ func Walk(x interface{}, f func(input string)) {
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			f(field.String())
-		}
-
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			Walk(field.Interface(), f)
 		}
 	}
