e63a6676e0ce608abb1ff3cd7bbf34a34b31ad5a
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-11 11:45:37 +0300

    fix map reordered failed tests

--- ch_12_reflection/walker_test.go
+++ ch_12_reflection/walker_test.go
@@ -71,14 +71,6 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
-		{
-			"maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
-		},
 	}
 
 	for _, test := range cases {
@@ -89,5 +81,32 @@ func TestWalk(t *testing.T) {
 			})
 			assert.Equal(t, test.ExpectedCalls, got)
 		})
+	}
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
