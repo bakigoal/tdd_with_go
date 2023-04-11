e3d5209a1c2f667b4bc3e50d6c3dadd4d04a6052
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-11 12:38:32 +0300

    use sync.Mutex to lock goroutine

--- ch_13_sync/counter.go
+++ ch_13_sync/counter.go
@@ -1,13 +1,18 @@
 package ch_13_sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
-	c.value += 1
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
 }
 
-func (c Counter) Value() int {
+func (c *Counter) Value() int {
 	return c.value
 }
--- ch_13_sync/counter_test.go
+++ ch_13_sync/counter_test.go
@@ -2,6 +2,7 @@ package ch_13_sync
 
 import (
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
@@ -13,5 +14,23 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 
 		assert.Equal(t, 3, counter.Value())
+	})
+	t.Run("it run safely concurrently", func(t *testing.T) {
+		counter := Counter{}
+		wantedCount := 1000
+
+		var wg sync.WaitGroup // A WaitGroup waits for a collection of goroutines to finish.
+		wg.Add(wantedCount)   // The main goroutine calls 'Add' to set the number of goroutines to wait for.
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done() // Then each of the goroutines calls 'Done' when finished.
+			}()
+		}
+
+		wg.Wait() // At the same time, 'Wait' can be used to block until all goroutines have finished.
+
+		assert.Equal(t, wantedCount, counter.Value())
 	})
 }
