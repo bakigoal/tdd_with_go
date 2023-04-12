5eaaec1a9bd757590acf3ffda082c07545aa86b2
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-04-12 09:51:03 +0300

    refactoring test

--- ch_15_roman_numerals/romans_test.go
+++ ch_15_roman_numerals/romans_test.go
@@ -2,11 +2,21 @@ package ch_15_roman_numerals
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
-	assert.Equal(t, want, got)
+	cases := []struct {
+		Num  int
+		Want string
+	}{
+		{1, "I"},
+	}
+
+	for _, testCase := range cases {
+		t.Run(strconv.Itoa(testCase.Num)+" == "+testCase.Want, func(t *testing.T) {
+			assert.Equal(t, testCase.Want, ConvertToRoman(testCase.Num))
+		})
+	}
 }
