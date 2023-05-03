798bac90445ee70c50bd3dbb3561b790ce96d6c1
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-03 16:57:10 +0300

    add tabs

--- ch_16_maths/clock.go
+++ ch_16_maths/clock.go
@@ -61,17 +61,17 @@ func newLine(w io.Writer) {
 
 func HourHand(w io.Writer, t time.Time) {
 	p := makeHand(hourInRadians(t), hourHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:7px;"/>`, p.X, p.Y)
+	fmt.Fprintf(w, `	<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:7px;"/>`, p.X, p.Y)
 }
 
 func MinuteHand(w io.Writer, t time.Time) {
 	p := makeHand(minutesInRadians(t), minuteHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:7px;"/>`, p.X, p.Y)
+	fmt.Fprintf(w, `	<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:7px;"/>`, p.X, p.Y)
 }
 
 func SecondHand(w io.Writer, t time.Time) {
 	p := makeHand(secondsInRadians(t), secondHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+	fmt.Fprintf(w, `	<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func makeHand(angle float64, length float64) Point {
@@ -89,6 +89,6 @@ const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 	 height="100%"
      viewBox="0 0 300 300">`
 
-const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+const bezel = `	<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
 
 const svgEnd = `</svg>`
