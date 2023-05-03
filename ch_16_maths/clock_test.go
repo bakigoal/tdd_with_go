c3014d453dec33b0642f1e67d10599d7674ecf34
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-03 15:45:08 +0300

    refactoring - add SVGWriter

--- ch_16_maths/clock_test.go
+++ ch_16_maths/clock_test.go
@@ -1,27 +1,13 @@
 package ch_16_maths
 
 import (
+	"bytes"
+	"encoding/xml"
 	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
 	"time"
 )
-
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 - 90}
-	got := SecondHand(tm)
-	assert.Equal(t, want, got)
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
-	assert.Equal(t, want, got)
-}
 
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
@@ -79,4 +65,50 @@ func roughlyEqualFloat64(a, b float64) bool {
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) &&
 		roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000"
+	y2 := "60.000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+}
+
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
