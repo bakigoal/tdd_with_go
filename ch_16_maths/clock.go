3aabf06bd53747c2341316836b3d8b370948331c
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-03 11:59:27 +0300

    add tests

---
+++ ch_16_maths/clock.go
@@ -0,0 +1,12 @@
+package ch_16_maths
+
+import "time"
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(duration time.Time) Point {
+	return Point{}
+}
---
+++ ch_16_maths/clock_test.go
@@ -0,0 +1,23 @@
+package ch_16_maths
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+	assert.Equal(t, want, got)
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+	assert.Equal(t, want, got)
+}
