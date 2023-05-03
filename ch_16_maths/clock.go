7c07b86e3c19aad1f717f77ac53e4eb1787f5e7b
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-03 14:32:53 +0300

    add SecondsInRadians

--- ch_16_maths/clock.go
+++ ch_16_maths/clock.go
@@ -1,12 +1,19 @@
 package ch_16_maths
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type Point struct {
 	X float64
 	Y float64
 }
 
-func SecondHand(duration time.Time) Point {
+func SecondHand(t time.Time) Point {
 	return Point{}
+}
+
+func SecondsInRadians(t time.Time) float64 {
+	return float64(t.Second()) * (math.Pi / 30)
 }
--- ch_16_maths/clock_test.go
+++ ch_16_maths/clock_test.go
@@ -2,6 +2,7 @@ package ch_16_maths
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 	"time"
 )
@@ -20,4 +21,31 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 	want := Point{X: 150, Y: 150 + 90}
 	got := SecondHand(tm)
 	assert.Equal(t, want, got)
+}
+
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 45), 3 * math.Pi / 2},
+		{simpleTime(0, 0, 7), 7 * math.Pi / 30},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := SecondsInRadians(c.time)
+			assert.Equal(t, c.angle, got)
+		})
+	}
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:01:05")
+}
+
+func simpleTime(hours int, minutes int, seconds int) time.Time {
+	return time.Date(1337, time.January, 1, hours, minutes, seconds, 0, time.UTC)
 }
