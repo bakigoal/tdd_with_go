e500a882026d3a9465a9ab74d5f02aa9460edc54
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-15 07:48:13 +0300

    use go-approval-tests

--- ch_18_html_templates/blogrenderer_test/blogrenderer_test.go
+++ ch_18_html_templates/blogrenderer_test/blogrenderer_test.go
@@ -2,9 +2,9 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"github.com/approvals/go-approval-tests"
 	"github.com/bakigoal/tdd_with_go/ch_17_reading_files/blogposts"
 	"github.com/bakigoal/tdd_with_go/ch_18_html_templates/blogrenderer"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -17,13 +17,15 @@ func TestRender(t *testing.T) {
 			Tags:        []string{"go", "tdd"},
 		}
 	)
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
-		assert.NoError(t, err)
-		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
-		got := buf.String()
-		assert.Equal(t, want, got)
+
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
 	})
 
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/bakigoal/tdd_with_go
 
 go 1.19
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
