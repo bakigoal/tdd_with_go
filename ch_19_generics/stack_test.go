d2c2f78eae85a170339f8ca73f26e8a1b86d36ab
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-05-16 08:01:32 +0300

    no need to test

--- ch_19_generics/stack_test.go
+++ ch_19_generics/stack_test.go
@@ -23,23 +23,4 @@ func TestStack(t *testing.T) {
 		assert.Equal(t, value, 123)
 		assert.True(t, myStackOfInts.IsEmpty())
 	})
-
-	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(Stack[string])
-
-		// check stack is empty
-		assert.True(t, myStackOfStrings.IsEmpty())
-
-		// add a thing, then check it's not empty
-		myStackOfStrings.Push("123")
-		assert.False(t, myStackOfStrings.IsEmpty())
-
-		// add another thing, pop it back again
-		myStackOfStrings.Push("456")
-		value, _ := myStackOfStrings.Pop()
-		assert.Equal(t, value, "456")
-		value, _ = myStackOfStrings.Pop()
-		assert.Equal(t, value, "123")
-		assert.True(t, myStackOfStrings.IsEmpty())
-	})
 }
